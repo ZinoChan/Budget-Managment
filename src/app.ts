96649a4a7c42d44bc87beef85c4c183eaa3cef34
ZinoChan <devzinochan@gmail.com>
2024-01-01 23:39:24 +0100

    init commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+class App {
+  public app: express.Application;
+  public port: string | number;
+  public env: string;
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 4001;
+    this.env = "development";
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`listening on port ${this.port}`);
+    });
+  }
+}
+
+export default App;
