4a30ac444f9a311ecdc81e022814ae4a924d1ef2
ZinoChan <devzinochan@gmail.com>
2024-01-17 20:58:30 +0100

    add data intraction utils

--- server/src/repositories/userRepository.ts
+++ server/src/repositories/userRepository.ts
@@ -1,5 +1,8 @@
+import redisClient from "@/lib/RedisClient";
 import { CreateUserInterface } from "@/types/auth.interface";
+import { signJwt } from "@/utils/jwt";
 import { Prisma, PrismaClient, User } from "@prisma/client";
+import config from "config";
 
 class UserRepository {
   private prisma: PrismaClient;
@@ -8,23 +11,50 @@ class UserRepository {
     this.prisma = prisma;
   }
 
-  async createUser(userData: CreateUserInterface): Promise<User> {
+  createUser = async (userData: CreateUserInterface): Promise<User> => {
     const user = await this.prisma.user.create({
       data: userData,
     });
     return user;
-  }
+  };
 
-  async findUniqueUser(where: Prisma.UserWhereUniqueInput) {
-    return (await this.prisma.user.findUnique({ where })) as User;
-  }
-  async updateUser(
+  findUniqueUser = async (
+    where: Prisma.UserWhereUniqueInput,
+    select?: Prisma.UserSelect
+  ) => {
+    return (await this.prisma.user.findUnique({
+      where,
+      select,
+    })) as User;
+  };
+  updateUser = async (
     where: Prisma.UserWhereUniqueInput,
     data: Prisma.UserUpdateInput,
     select?: Prisma.UserSelect
-  ) {
+  ) => {
     return (await this.prisma.user.update({ where, data, select })) as User;
-  }
+  };
+
+  signTokens = async (user: Prisma.UserCreateInput) => {
+    // 1. Create Session
+    redisClient.set(`${user.id}`, JSON.stringify(user), {
+      EX: config.get<number>("redisCacheExpiresIn") * 60,
+    });
+
+    const access_token = signJwt({ sub: user.id }, "jwtAccessTokenPrivateKey", {
+      expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
+    });
+
+    const refresh_token = signJwt(
+      { sub: user.id },
+      "jwtRefreshTokenPrivateKey",
+      {
+        expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
+      }
+    );
+
+    return { access_token, refresh_token };
+  };
 }
 
 export default UserRepository;
